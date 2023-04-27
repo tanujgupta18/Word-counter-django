40d712713eb69d0a040f97d8b659f52a282f4fb3
Tanuj Gupta <64638288+Tanujgupta1872003@users.noreply.github.com>
2023-04-28 00:53:08 +0530

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from .models import Feature
+
+# Create your views here.
+
+def index(request):
+    features = Feature.objects.all()
+    return render(request,'index.html', {'features': features})
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request,'counter.html' , {'amount': amount_of_words})
